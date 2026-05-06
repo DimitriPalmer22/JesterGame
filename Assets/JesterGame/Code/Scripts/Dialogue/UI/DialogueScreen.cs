feec6700928af175afff327482b5d47d20cb208f
DimitriPalmer22 <dimitriPalmer22@gmail.com>
2026-05-05 21:35:20 -0400

    Dialogue screen UI class created

---
+++ Assets/JesterGame/Code/Scripts/Dialogue/UI/DialogueScreen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnrealToUnity.Code.Scripts.Core.UserInterface;
+
+namespace JesterGame.Code.Scripts.Dialogue.UI
+{
+    public class DialogueScreen : UIScreen
+    {
+        protected override void CustomInitialize()
+        {
+        }
+
+        protected override void CustomStart()
+        {
+        }
+
+        protected override IEnumerator OpenScreenCoroutine()
+        {
+            yield return null;
+        }
+
+        protected override IEnumerator CloseScreenCoroutine()
+        {
+            yield return null;
+        }
+    }
+}
